612024080a7cd64e38d681cbaff867c2e0e535a9
Laya <162989029+pgl-reddy@users.noreply.github.com>
2026-04-20 18:30:00 +0530

    LSTM model for sentiment analysis using IMDB data

---
+++ Sentiment_Analysis_LSTM.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+# Load + preprocess
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(num_words=5000)
+
+x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=100)
+x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=100)
+
+print("Training samples:", len(x_train))
+print("Test samples:", len(x_test))
+
+# Model (simplified)
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(5000, 32),
+    tf.keras.layers.LSTM(32),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+# Compile + train
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=3, validation_split=0.2)
+
+# Test
+print("Test Acc:", model.evaluate(x_test, y_test)[1])
